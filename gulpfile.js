a44cde142800f83691fe871f5dda9779dab47733
Jean Lucas <jeanleonino@gmail.com>
2015-11-05 00:00:49 -0300

    Added jshint as gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,15 @@ var gutil = require('gulp-util');
 gulp.task('default', function(){
   return gutil.log('Gulp is running')
 });
+
+gulp.task('move', function(){
+  // copy files from source to public folder
+  gulp.src('/*.html').pipe(gulp.dest('public'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
